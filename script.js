4e538ece74cc6e3a7003a7f47c2c35855ea95340
Artem <31312136+wreckage7@users.noreply.github.com>
2018-02-08 18:09:33 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+
+
+function fun1() {
+    var checkbox;
+    checkbox = document.getElementById("one");
+
+    if(checkbox.checked){
+        alert("Checked");
+    } else {
+        alert("Unchecked");
+    }
+}
